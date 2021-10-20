1e142a95b2a7d5b638c786cf1b168abfa1cfe882
Eric Bishard <e.bishard@gmail.com>
2021-10-19 22:13:26 -0700

    Add comment

--- src/server.js
+++ src/server.js
@@ -8,7 +8,10 @@ const startApiServer = async() => {
       username: process.env.CB_USER,
       password: process.env.CB_PASS,
     })
+    // By default start function will wait for indexes, but you can disabled it setting ignoreWatchIndexes to true. 
+    // It's not required to execute the start method in order for Ottoman work. 
     await ottoman.start()
+    
     const port = process.env.APP_PORT
     app.listen(port, () => {
       console.log(`API started at http://localhost:${port}`)
