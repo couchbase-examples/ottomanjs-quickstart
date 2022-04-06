98c093874e59c79288247cb11ccd883ab02ff94d
ejscribner <ejscribner@gmail.com>
2022-04-06 16:38:11 -0700

    added capella connection

--- src/server.js
+++ src/server.js
@@ -2,12 +2,20 @@ import { app, ottoman } from './app.js'
 
 const startApiServer = async() => {
   try {
+    // await ottoman.connect({
+    //   bucketName: process.env.CB_BUCKET,
+    //   connectionString: 'couchbase://' + process.env.CB_URL,
+    //   username: process.env.CB_USER,
+    //   password: process.env.CB_PASS,
+    // })
+
     await ottoman.connect({
       bucketName: process.env.CB_BUCKET,
-      connectionString: process.env.CB_URL,
+      connectionString: 'couchbases://' + process.env.CB_URL + '?ssl=no_verify',
       username: process.env.CB_USER,
       password: process.env.CB_PASS,
     })
+
     // By default start function will wait for indexes, but you can disabled it setting ignoreWatchIndexes to true. 
     // It's not required to execute the start method in order for Ottoman work. 
     await ottoman.start()
