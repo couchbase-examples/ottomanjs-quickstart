426ca5bc7cb14ccb30083c0c26b36918327a31eb
ejscribner <ejscribner@gmail.com>
2022-04-08 13:43:44 -0700

    added `IS_CAPELLA` env var

--- src/server.js
+++ src/server.js
@@ -1,20 +1,30 @@
 import { app, ottoman } from './app.js'
 
+const IS_CAPELLA = process.env.IS_CAPELLA
+
+if (!IS_CAPELLA) {
+  throw new Error(
+      'Please define the IS_CAPELLA environment variable inside dev.env. \nSet to \`true\` if you are connecting to a Capella cluster, and \`false\` otherwise.\n'
+  )
+}
+
 const startApiServer = async() => {
   try {
-    // await ottoman.connect({
-    //   bucketName: process.env.CB_BUCKET,
-    //   connectionString: 'couchbase://' + process.env.CB_URL,
-    //   username: process.env.CB_USER,
-    //   password: process.env.CB_PASS,
-    // })
-
-    await ottoman.connect({
-      bucketName: process.env.CB_BUCKET,
-      connectionString: 'couchbases://' + process.env.CB_URL + '?ssl=no_verify',
-      username: process.env.CB_USER,
-      password: process.env.CB_PASS,
-    })
+    if (IS_CAPELLA === 'true') {
+      await ottoman.connect({
+        bucketName: process.env.CB_BUCKET,
+        connectionString: 'couchbases://'+ process.env.CB_URL +'ssl=no_verify',
+        username: process.env.CB_USER,
+        password: process.env.CB_PASS,
+      })
+    } else {
+      await ottoman.connect({
+        bucketName: process.env.CB_BUCKET,
+        connectionString: 'couchbase://' + process.env.CB_URL,
+        username: process.env.CB_USER,
+        password: process.env.CB_PASS,
+      })
+    }
 
     // By default start function will wait for indexes, but you can disabled it setting ignoreWatchIndexes to true. 
     // It's not required to execute the start method in order for Ottoman work. 
